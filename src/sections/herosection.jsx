86d5992a341c016a7a24c2888a4e3d8e31f37dd7
Raghavcpp <raghav.cpp@gmail.com>
2025-05-29 21:59:32 +0530

    lazy loading

--- src/sections/herosection.jsx
+++ src/sections/herosection.jsx
@@ -3,6 +3,9 @@ import image from "../assets/white-nature-leaves-minimal-back.png"; // Correct p
 import Navbar from './navbar';
 import { words, imgpath } from '../assets/constants';
 import {Drawer,Card} from "./drawer.jsx";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 
 const HeroSection = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -10,7 +13,7 @@ const HeroSection = () => {
     <>
       <Navbar setIsOpen={setIsOpen}/>
       <div id="home"
-        className="mt-20 md:mt-0 relative w-full h-screen flex items-center justify-center"
+        className="mt-2  relative w-full h-screen flex items-center justify-center"
         style={{ backgroundImage: `url(${image})`, backgroundSize: "cover", backgroundPosition: "center" }}
       >
         {/* Overlay */}
@@ -22,7 +25,7 @@ const HeroSection = () => {
               {words.map((word, index) => (
                 <span key={index} className="flex items-center gap-2 has-appear" style={{ animationDelay: `${index * 0.2}s` }}>
                   {word}
-                  {imgpath[index] && <img src={`/images/${imgpath[index]}`} className="h-20 inline" alt={`yoga-${index}`} />}
+                  {imgpath[index] && <LazyLoadImage effect="blur" src={`/images/${imgpath[index]}`} className="h-20 inline" alt={`yoga-${index}`} />}
                 </span>
               ))}
             </h1>
@@ -40,10 +43,10 @@ const HeroSection = () => {
       </Drawer>
       <div className="grade bg-linear-to-b from-[#FAFAFA] to-[#F7EDE4] h-10 md:h-30"></div>
       <div className="relative flex center flex-row flex-wrap justify-evenly text-center w-screen">
-        <img src="900 x 1284/h2-img1.jpg" alt="h2-img1.png" data-aos="fade-right" className="rounded-tl-full max-h-100" />
-        <img src="900 x 1284/h2-img2.jpg" alt="h2-img2.png" data-aos="fade-up" className="rounded-t-full max-h-100" />
-        <img src="900 x 1284/h2-img3.jpg" alt="h2-img3.png" data-aos="fade-up" className="rounded-t-full max-h-100" />
-        <img src="900 x 1284/h2-img4.jpg" alt="h2-img4.png" data-aos="fade-left" className="rounded-tr-full max-h-100" />
+        <LazyLoadImage effect="blur" src="900 x 1284/h2-img1.jpg" alt="h2-img1.png"  data-aos="fade-right" className="rounded-tl-full max-h-100" />
+        <LazyLoadImage effect="blur" src="900 x 1284/h2-img2.jpg" alt="h2-img2.png"  data-aos="fade-up" className="rounded-t-full max-h-100" />
+        <LazyLoadImage effect="blur" src="900 x 1284/h2-img3.jpg" alt="h2-img3.png"  data-aos="fade-up" className="rounded-t-full max-h-100" />
+        <LazyLoadImage effect="blur" src="900 x 1284/h2-img4.jpg" alt="h2-img4.png"  data-aos="fade-left" className="rounded-tr-full max-h-100" />
       </div>
     </>
   );
